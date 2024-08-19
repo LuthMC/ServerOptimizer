fb3a392632845cb958a9ac4896d7806a538f8785
LuthMC <luthfi366822@gmail.com>
2024-08-19 14:42:50 +0700

    Add ClearCommand.php

---
+++ src/Luthfi/ServerOptimizer/commands/ClearCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Luthfi\ServerOptimizer\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use Luthfi\ServerOptimizer\Main;
+
+class ClearCommand extends Command {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        parent::__construct("serveroptimizer clear", "Perform a quick lag cleanup", "/serveroptimizer clear", ["soclear"]);
+        $this->plugin = $plugin;
+        $this->setPermission("serveroptimizer.clear");
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+
+        $level = $this->plugin->getServer()->getLevels()[0];
+        $count = 0;
+
+        foreach ($level->getEntities() as $entity) {
+            if (!$entity instanceof Player && !$entity instanceof NPC) {
+                $entity->close();
+                $count++;
+            }
+        }
+
+        $sender->sendMessage(TF::GREEN . "Cleared " . $count . " entities.");
+
+        return true;
+    }
+}
